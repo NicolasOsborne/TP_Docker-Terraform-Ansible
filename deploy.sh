fc7bfa7ef68cb3605967e09ddbbfa6208b180ee7
Nicolas Osborne <nho.osborne@gmail.com>
2026-02-06 17:16:41 +0100

    Fix wait for services to be up before listing them + Add Certbot (production ready)

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,25 @@
+#!/bin/bash
+
+set -e
+
 echo "--- DEPLOYMENT START ---"
 
 echo "--- TERRAFORM INIT ---"
 cd terraform
 terraform init
 terraform apply -auto-approve
+cd ..
 
 echo "--- ANSIBLE DEPLOY ---"
-cd ../ansible
+cd ansible
 ansible-playbook -i inventory.ini playbook.yml
+cd ..
+
+echo "--- WAITING FOR SERVICES TO START (this may take some time) ---"
+while [ "$(docker stack services tp_devops | grep -c "0/")" -gt 0 ]; do
+    echo -n "."
+    sleep 3
+done
 
 echo "--- DEPLOYMENT DONE ---"
-docker service ls || echo "Docker services check failed.."
+docker stack services tp_devops
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo "--- PREPARE SYSTEM ---"
 sudo apt update
 
